f8c2e73da25bfc3d83466c36b2c4f7026f93814e
HOA <n/a>
2025-09-15 13:39:44 +0200

    3AKIF POS Klasse Auto

---
+++ 34akif_pos/2025-09-15__Student_Unterricht/Auto.java
@@ -0,0 +1,30 @@
+public class Auto {
+    private String marke;
+    private int kilometer;
+    private boolean automatik;
+    
+    public String getMarke() {
+        return marke;
+    }
+    
+    public void setMarke(String neueMarke) {
+        marke = neueMarke;
+    }
+    
+    public int getKilometer() {
+        return kilometer;
+    }
+    
+    public void setKilometer(int neueKilometer) {
+        kilometer = neueKilometer;
+    }
+    
+    public boolean getAutomatik() {
+        return automatik;
+    }
+    
+    public void setAutomatik(boolean neuAutomatik) {
+        automatik = neuAutomatik;
+    }
+    
+}
