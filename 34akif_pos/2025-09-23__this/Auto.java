41f416426606e044a9280899824f69616e4742e2
HOA <n/a>
2025-09-23 14:54:04 +0200

    this Schlüsselwort

---
+++ 34akif_pos/2025-09-23__this/Auto.java
@@ -0,0 +1,80 @@
+public class Auto {
+    
+    // (Objekt-)Eigenschaften
+    // Attribute
+    // Instanzvariablen
+    private String marke;
+    private int kilometer;
+    private boolean automatik;
+    
+    // Parameterloser Konstruktor
+    public Auto() {
+        setMarke("Mazda");
+        setKilometer(10);
+        setAutomatik(true);
+    }
+    
+    // Konstruktor mit Parametern
+    public Auto(String neueMarke, int neueKilometer, boolean neuAutomatik) {
+        setMarke(neueMarke);
+        setKilometer(neueKilometer);
+        setAutomatik(neuAutomatik);
+    }
+    
+    // Methoden
+    public String getMarke() {
+        return marke;
+    }
+    
+    public void setMarke(String neueMarke) {
+        marke = neueMarke;
+    }
+    
+    public int getKilometer() {
+        return kilometer;
+    }
+    
+    public void setKilometer(int neueKilometer) {
+        kilometer = neueKilometer;
+    }
+    
+    public boolean getAutomatik() {
+        return automatik;
+    }
+    
+    public void setAutomatik(boolean neuAutomatik) {
+        automatik = neuAutomatik;
+    }
+    
+    public void printAuto() {
+        // System.out.println("Auto: " + getMarke() + ", km: " + getKilometer());
+        
+        // Beispiele: Auto: Skoda, km: 23000 (Automatik)"
+        //            Auto: Ferrari, km: 90000 (Handschaltung)"
+        
+        // Ideenskizze
+        
+        // Wenn Eigenschafte automatik true ist
+        //    System.out.println(...."(Automatik)");
+        // Sonst (automatik ist false)
+        //    System.out.println(..."(Handschaltung)");
+        
+        if (automatik == true) { // if-Bedingung
+            System.out.println("Auto: " + getMarke() + ", km: " + getKilometer() + " (Automatik)");
+        } else {
+            System.out.println("Auto: " + getMarke() + ", km: " + getKilometer() + " (Handschaltung)");
+        }
+        
+        // Achtung !!!!!!!!!!!!!!!!
+        // =  ... Zuweisung
+        // == ... Vergleich
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ 34akif_pos/2025-09-23__this/Unterrichtsfach.java
@@ -0,0 +1,11 @@
+
+public class Unterrichtsfach
+{
+   
+   private String namen;
+   private String lehrer;
+   private int wochenStundenAnzahl;
+   private boolean freifach; // boolean: true oder false
+   
+   // TODO
+}
