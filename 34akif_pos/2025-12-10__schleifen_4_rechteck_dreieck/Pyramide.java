25cf7e690be3012da5de9b7a61798e8d97b9eb14
HOA <n/a>
2025-12-10 15:21:17 +0100

    3AKIF: Schleifen (Pyramide)

--- 34akif_pos/2025-12-10__schleifen_4_rechteck_dreieck/Pyramide.java
+++ 34akif_pos/2025-12-10__schleifen_4_rechteck_dreieck/Pyramide.java
@@ -1,9 +1,9 @@
 
-/*
-     @
-    @@@
-   @@@@@
-  @@@@@@@
+/*              
+   @       3
+  @@@      2
+ @@@@@     1
+@@@@@@@    0
   
   Schritt 1:
 @
@@ -32,7 +32,24 @@ public class Pyramide {
         }
     }
     
+/*       hoehe=4
+ 
+   @       3    h=0   hoehe-h-1
+  @@@      2    h=1
+ @@@@@     1    h=2
+@@@@@@@    0    h=3
+*/
     public void zeichnen() {
-        // TODO
+        int breite = 1;
+        for (int h=0; h<hoehe; h++) {
+            for (int l=0; l<hoehe-h-1; l++) {
+                System.out.print("_");
+            }
+            for (int b=0; b<breite; b++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            breite += 2;
+        }
     }
 }
